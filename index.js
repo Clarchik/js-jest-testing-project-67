489d858950698ab251e84427eb41a2e5ea6905d9
dtnfkmdtnfkm3@mail.ru <Clarchik>
2023-09-02 11:17:09 +0200

    ss

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-import * as Axios from "axios";
-import * as fs from 'fs';
+const { Axios } = require('axios');
+const fs = require('fs');
 
-export async function loadPage(dir, url) {
+async function loadPage(dir, url) {
   const fileName = url.split('//')[1].replace(/[.\/]/g, '-');
 
-  const { data } = await new Axios.Axios({ method: 'get' }).get(url);
+  const { data } = await new Axios({ method: 'get' }).get(url);
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir, { recursive: true });
   }
   fs.writeFileSync(`${dir}/${fileName}.html`, data);
+}
+
+module.exports = {
+  loadPage
 }
